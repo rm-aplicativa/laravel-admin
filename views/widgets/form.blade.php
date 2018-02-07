494d347f7cfcd8dcd436c4326136c2d5c46469fc
Ricardo Madriz <rmadriz@outcoding.com>
2018-02-07 14:15:29 -0600

    Fix column width in form widget

--- views/widgets/form.blade.php
+++ views/widgets/form.blade.php
@@ -12,12 +12,7 @@
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="col-sm-2">
         </div>
-        <div class="col-sm-2">
-            <div class="btn-group pull-left">
-                <button type="reset" class="btn btn-warning pull-right">{{ trans('admin::lang.reset') }}</button>
-            </div>
-        </div>
-        <div class="col-sm-4">
+        <div class="col-sm-8">
             <div class="btn-group pull-right">
                 <button type="submit" class="btn btn-info pull-right">{{ trans('admin::lang.submit') }}</button>
             </div>
