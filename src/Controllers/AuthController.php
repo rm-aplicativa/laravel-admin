cec572fff4391da82b95a3a11f4312dd66bfc28d
Ricardo Madriz <rmadriz@outcoding.com>
2018-04-03 09:08:05 -0600

    Add missing namespace use declaration

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
